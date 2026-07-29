b7cb0f1b25d10c7053d695b7c0bc1798fce54cb3
Nidhi Panchal <2025ucp1548@mnit.ac.in>
2026-07-29 23:49:51 +0530

    Time: 0 ms (100%), Space: 13.8 MB (48.41%) - LeetHub

---
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) {
+        int max1 = -1;
+        int max2 = -1;
+        int maxIdx = -1;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] > max1) {
+                max2 = max1;
+                max1 = nums[i];
+                maxIdx = i;
+            } else if (nums[i] > max2) {
+                max2 = nums[i];
+            }
+        }
+
+        return (max1 >= 2 * max2) ? maxIdx : -1;
+    }
+};
