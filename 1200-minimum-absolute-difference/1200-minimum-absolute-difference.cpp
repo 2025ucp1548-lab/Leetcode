526a9e335439408e74aa31e4184d51433b408575
Nidhi Panchal <2025ucp1548@mnit.ac.in>
2026-08-29 22:09:35 +0530

    Time: 19 ms (25.37%), Space: 36.9 MB (17.1%) - LeetHub

---
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        std::sort(arr.begin(), arr.end());
+        
+        std::vector<std::vector<int>> result;
+        int min_diff = INT_MAX;
+        for (size_t i = 1; i < arr.size(); ++i) {
+            int diff = arr[i] - arr[i - 1];
+            if (diff < min_diff) {
+                min_diff = diff;
+                result.clear();
+                result.push_back({arr[i - 1], arr[i]});
+            } else if (diff == min_diff) {
+                result.push_back({arr[i - 1], arr[i]});
+            }
+        }
+        return result;
+    }
+};
