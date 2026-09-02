9f3ba85b9f51da986ffd5f40efb4bcf1632287c5
Nidhi Panchal <2025ucp1548@mnit.ac.in>
2026-09-02 23:45:37 +0530

    Time: 0 ms (100%), Space: 13.9 MB (98.7%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        int total = 1 << n; // 2^n
+        std::vector<int> result(total);
+        
+        for (int i = 0; i < total; ++i) {
+            result[i] = i ^ (i >> 1);
+        }
+        
+        return result;
+    }
+};   
